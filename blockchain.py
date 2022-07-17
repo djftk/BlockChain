2142fc954d455faaaf3f0d844f7b9e6a0001f6ce
Leesungsup <tmdtjq0116@naver.com>
2022-07-18 01:14:02 +0900

    Block

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,14 @@
-print("Test")
+import hashlib
+class Block():
+    current_transaction = dict()
+    def __init__(self, id, timestamp, data):
+        self.new_transaction(data["sender"],data["recipient"],data["amount"])
+        self.id = hex(id)
+        self.timestamp = timestamp
+        self.data = self.current_transaction
+        self.previousHash = 0
+        self.hash = self.calHash()
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transaction[hashlib.sha256(hex(len(self.current_transaction)).encode()).hexdigest()[:40]]={'sender' : sender, 'recipient' : recipient, 'amount' : amount}
+    def calHash(self):
+        return hashlib.sha256(str(self.id).encode()).hexdigest()[:40]
